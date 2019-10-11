1fdae5f41dfddac786049c21eef83aabf0deb9a7
Saurabh Rajguru <saurabh.rajguru@dynamicelements.no>
2019-10-11 17:29:18 +0530

    WIP - Added Ununsed local variable Analyzer and codefix.

---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer/Analyzers/UnusedLocalVariable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace IntelliTectAnalyzer.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UnusedLocalVariable : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "INTL0004";
+        private const string Title = "Unused Local Variable";
+        private const string MessageFormat = "Unused local variables should be removed";
+        private const string Description = "If a local variable is declared but not used, it is dead code and should be removed.";
+        private const string Category = "Performance";
+        private const string HelpLinkUri = "https://github.com/IntelliTect/CodingStandards";
+
+        private static readonly DiagnosticDescriptor _Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+           Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context is null)
+            {
+                throw new System.ArgumentNullException(nameof(context));
+            }
+
+            context.RegisterSyntaxNodeAction(AnalyzeUnUnsedLocal, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void AnalyzeUnUnsedLocal(SyntaxNodeAnalysisContext context)
+        {
+            var method = context.Node as MethodDeclarationSyntax;
+
+            var dataFlow = context.SemanticModel.AnalyzeDataFlow(method.Body);
+
+            var variablesDeclared = dataFlow.VariablesDeclared;
+            var variablesRead = dataFlow.ReadInside.Union(dataFlow.ReadOutside);
+            var unused = variablesDeclared.Except(variablesRead);
+
+            if (unused.Any())
+            {
+                foreach (var unusedVar in unused)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(_Rule, unusedVar.Locations.First()));
+                }
+            }
+        }
+
+    }
+}
---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer/CodeFixes/UnusedLocalVariable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace IntelliTectAnalyzer.CodeFixes
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(NamingPropertyPascal)), Shared]
+    public class UnusedLocalVariable : CodeFixProvider
+    {
+        private const string Title = "Remove Unused variable";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(Analyzers.UnusedLocalVariable.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+        }
+    }
+}
